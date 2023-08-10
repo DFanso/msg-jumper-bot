dcbd8d2b2c8d782ef3aa06d3537efd2c2af71ec9
Leo Gavin <leogavin123@outlook.com>
2023-08-10 11:43:22 +0530

    Update bot-V2.js

--- bot-V2.js
+++ bot-V2.js
@@ -9,98 +9,36 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
   ]
 });
-const token = 'MTEzODc2MDMwNzc3NTM3NzQ1OQ.GZhY8S.-t8Rns6EwZYXxnVAim-07aWLlkH2kOuhBZZqO4';
+const token = 'Bot token';
 
 const channelPairs = [
-    //trello
-
+    //add many servers and channels as you want
     {
-      server1Id: '1126776340528517120', // sales teams
-      server2Id: '1123181731906850866', // models
-      channel1Id: '1126780648565256242', // sales teams (becky)
-      channel2Id: '1123229303677669456', // models (becky)
+      server1Id: 'Server 1 ID', 
+      server2Id: 'Server 2 ID', 
+      channel1Id: 'Channel 1 ID', 
+      channel2Id: 'Channel 2 ID', 
     },
     {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780670929285130', // models (mya)
-        channel2Id: '1127855413421940806', // sales teams (mya)
+      server1Id: 'Server 1 ID', 
+      server2Id: 'Server 2 ID', 
+      channel1Id: 'Channel 1 ID', 
+      channel2Id: 'Channel 2 ID', 
     },
     {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780707365191712', // sale teams (scarlett)
-        channel2Id: '1127926306407985233', // models (scarlett)
+      server1Id: 'Server 1 ID', 
+      server2Id: 'Server 2 ID', 
+      channel1Id: 'Channel 1 ID', 
+      channel2Id: 'Channel 2 ID', 
     },
     {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780740353396736', // sale teams (mariana)
-        channel2Id: '1127948690137563218', // models (mariana)
+      server1Id: 'Server 1 ID', 
+      server2Id: 'Server 2 ID', 
+      channel1Id: 'Channel 1 ID', 
+      channel2Id: 'Channel 2 ID', 
     },
-    {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780778395750531', // sale teams (catalina)
-        channel2Id: '1127949039346909235', // models (catalina)
-    },
-    {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780816299659316', // sale teams (bella)
-        channel2Id: '1127949078244892735', // models (bella)
-    },
-    {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1126780863171010600', // sale teams (honey-lulu)
-        channel2Id: '1127949140706476064', // models (honey-lulu)
-    },
-
-    //top fans
-
-    {
-        server1Id: '1126776340528517120', // sales teams
-        server2Id: '1123181731906850866', // models
-        channel1Id: '1127475303393001552', // sales teams (becky)
-        channel2Id: '1127489313232392243', // models (becky)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475400176574535', // models (mya)
-          channel2Id: '1127855315333939222', // sales teams (mya)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475471387475989', // sale teams (scarlett)
-          channel2Id: '1127926388503093288', // models (scarlett)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475597875101748', // sale teams (mariana)
-          channel2Id: '1127949599303282789', // models (mariana)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475664463867975', // sale teams (catalina)
-          channel2Id: '1127949638457110569', // models (catalina)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475746366046318', // sale teams (bella)
-          channel2Id: '1127949673844445194', // models (bella)
-      },
-      {
-          server1Id: '1126776340528517120', // sales teams
-          server2Id: '1123181731906850866', // models
-          channel1Id: '1127475836015087696', // sale teams (honey-lulu)
-          channel2Id: '1127949730840838194', // models (honey-lulu)
-      },
+    
+    
     
   ];
 
